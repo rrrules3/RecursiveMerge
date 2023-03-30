fd914b20fb959dc61f48c5bdcfda0309b0e95511
Rohan <rohan.rajesh1205@gmail.com>
2023-03-29 22:49:38 -0400

    Readability
    Made a few changes for readability

--- src/main/java/com/mycompany/Recursive/RecursiveMergeSort.java
+++ src/main/java/com/mycompany/Recursive/RecursiveMergeSort.java
@@ -14,23 +14,23 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 public class RecursiveMergeSort {
-    public static void merge(int[] a, int[] leftside, int[] rightside, int left, int right) {
+    public static void merge(int[] a, int[] leftarr, int[] rightarr, int left, int right) {
     int i = 0;
     int j = 0;
     int k = 0;
     while (i < left && j < right) {
-        if (leftside[i] <= rightside[j]) {
-            a[k++] = leftside[i++];
+        if (leftarr[i] <= rightarr[j]) {
+            a[k++] = leftarr[i++];
         }
         else {
-            a[k++] = rightside[j++];
+            a[k++] = rightarr[j++];
         }
     }
     while (i < left) {
-        a[k++] = leftside[i++];
+        a[k++] = leftarr[i++];
     }
     while (j < right) {
-        a[k++] = rightside[j++];
+        a[k++] = rightarr[j++];
     }
 }
     public static void mergeSort(int[] a, int n) {
@@ -51,8 +51,8 @@ public class RecursiveMergeSort {
     mergeSort(rightside, n - mid);
 
     merge(a, leftside, rightside, mid, n - mid);
-} void printArray(int[] arr)
-{
+} 
+    void printArray(int[] arr){
     //this method helps us print out the array
     System.out.print("{");
     for (int i = 0; i < arr.length; i++){
@@ -63,28 +63,33 @@ public class RecursiveMergeSort {
 }
     
      public static void main(String args[]){
-         //Initializes Scanner
-         Scanner scan = new Scanner(System.in);
-         System.out.println("How many elements do you want to test?");
-         int input = scan.nextInt();
-         Random randomize = new Random();
-         int[] array = new int[input];
-         //Sets random integers between 1 and 1000 in the array
-         for (int i = 0; i < array.length; i++) {
-			array[i] = randomize.nextInt(100);
-		}
+        //Initializes Scanner
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many elements do you want to test?");
+        //user inputs amount of elements they want in the array
+        int input = scan.nextInt();
+        Random randomize = new Random();
+        int[] array = new int[input];
+        //Sets random integers between 1 and 1000 in the array
+        for (int i = 0; i < array.length; i++) {
+		array[i] = randomize.nextInt(100);
+	}
          
-         RecursiveMergeSort tester = new RecursiveMergeSort();
-         System.out.println("Unsorted:");
-         tester.printArray(array);
-         double start = System.nanoTime();
-         tester.mergeSort(array, input);
-         System.out.println("Sorted:");
-         double end = System.nanoTime();
-         double total = end - start;
-         tester.printArray(array);
-         System.out.println("It took " + total + " nanoseconds.");
+        RecursiveMergeSort tester = new RecursiveMergeSort();
+        //Prints out unsorted array
+        System.out.println("Unsorted:");
+        tester.printArray(array);
+        //starts a timer for how long the sort takes in nanoseconds and ends it when complete
+        double start = System.nanoTime();
          
+        tester.mergeSort(array, input);
+         
+        double end = System.nanoTime();
+        double total = end - start;
+        //prints out the sorted array as well as the time
+        System.out.println("Sorted:");
+        tester.printArray(array);
+        System.out.println("It took " + total + " nanoseconds.");
          
      }
 }
